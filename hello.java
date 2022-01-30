6821868ad58119fbcfb8e189b772a26bf667eea8
poojauttam122 <98681730+poojauttam122@users.noreply.github.com>
2022-01-30 12:10:46 +0530

    update hello.java with second line message

--- hello.java
+++ hello.java
@@ -1,5 +1,6 @@
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World! My name is Pooja"); 
+        System.out.println("Hi, This is my second line");
     }
 }
